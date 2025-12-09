1f49130d992766693515200c2cb5ddcbe8e170bb
Nathan-N7 <nathan.rod1710@gmail.com>
2025-12-08 21:19:40 -0300

    cadastrando e mostrando nossos clientes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,58 @@
+import  {useEffect, useState, useRef, type FormEvent} from 'react'
 import { FiTrash } from 'react-icons/fi'
+import {api} from './services/api'
+
+
+interface customerProps{
+  id: string;
+  name: string;
+  email: string;
+  status: boolean;
+}
 
 export default function App() {
+  const [customers, setCustomers] = useState<customerProps[]>([])
+
+  const nameRef = useRef<HTMLInputElement | null>(null);
+  const emailRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    loadCustomers();
+  }, [])
+
+  async function loadCustomers() {
+    const response = await api.get("/customers")
+    setCustomers(response.data);
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    if (!nameRef.current?.value || !emailRef.current?.value) return;
+    const response = await api.post("/customer", {
+      name: nameRef.current?.value,
+      email: emailRef.current?.value
+    })
+  }
+
   return (
     <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
-      <main className="my-10 w-full md:max-w-2x1">
+      <main className="my-10 w-full md:max-w-2xl">
         <h1 className="text-4xl font-medium text-white">Clientes</h1>
 
-        <form className="flex flex-col my-6">
+        <form className="flex flex-col my-6" onSubmit={handleSubmit}>
           <label className="font-medium text-white">Nome:</label>
           <input
           type="text"
           placeholder="Digite seu nome completo..."
           className="w-full mb-5 p-2 rounded"
+          ref={nameRef}
           />
           <label className="font-medium text-white">Email:</label>
           <input
           type="text"
           placeholder="Digite seu email..."
           className="w-full mb-5 p-2 rounded"
+          ref={emailRef}
           />
           <input 
           type="submit" 
@@ -26,16 +61,18 @@ export default function App() {
           />
         </form>
 
-        <section className="flex flex-col">
-          <article className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
-            <p><span className="font-medium">Name:</span></p>
-            <p><span className="font-medium">Email:</span></p>
-            <p><span className="font-medium">Status:</span></p>
+        <section className="flex flex-col gap-4">
+          {customers.map( (customer) => (
+            <article key={customer.id} className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
+            <p><span className="font-medium">Name:</span>{customer.name}</p>
+            <p><span className="font-medium">Email:</span>{customer.email}</p>
+            <p><span className="font-medium">Status:</span>{customer.status ? "ATIVO" : "INATIVO"}</p>
 
             <button className='bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-8 -top-2'>
               <FiTrash size={18} color="#fff"/>
             </button>
           </article>
+          ))}
         </section>
 
       </main>
